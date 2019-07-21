9a54c91105611da2a17dd5fefb71fa865a3bb48e
Amy Kim <amesk3@gmail.com>
2019-07-21 18:26:10 -0400

    added material-ui to listview with grid

--- client/src/components/ListView.js
+++ client/src/components/ListView.js
@@ -1,7 +1,18 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import GridListTileBar from "@material-ui/core/GridListTileBar";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import IconButton from "@material-ui/core/IconButton";
+import InfoIcon from "@material-ui/icons/Info";
+// import tileData from "./tileData";
+
 import { fetchProducts } from "../store/productsReducer";
+
+
 
 export class ListView extends Component {
   componentDidMount() {
@@ -13,6 +24,22 @@ export class ListView extends Component {
       this.props.fetchProducts(this.props.category);
     }
   }
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    overflow: "hidden",
+    backgroundColor: theme.palette.background.paper
+  },
+  gridList: {
+    width: 500,
+    height: 450
+  },
+  icon: {
+    color: "rgba(255, 255, 255, 0.54)"
+  }
+}));
 
   render() {
     if (!this.props.products) {
@@ -23,31 +50,50 @@ export class ListView extends Component {
         </div>
       );
     }
-    const imgStyle = {
-      width: "300px",
-      height: "300px"
-    };
+    const useStyles = makeStyles(theme => ({
+        root: {
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "space-around",
+          overflow: "hidden",
+          backgroundColor: theme.palette.background.paper
+        },
+        gridList: {
+          width: 500,
+          height: 450
+        },
+        icon: {
+          color: "rgba(255, 255, 255, 0.54)"
+        }
+      }));
+    const classes = useStyles();
+    console.log("working until return");
     return (
       <div>
         <h2 id="categoryTitle">Our {this.props.category}</h2>
-        <div className="parentCategory">
-          {this.props.products.map(product => (
-            <div className="one_category" key={product.id}>
-              <img
-                className="imageProduct"
-                src={product.image}
-                style={imgStyle}
-              />
-              <p className="nameProduct">{product.name}</p>
-              <p className="priceProduct">{product.price} $</p>
-              <Link
-                to={`/${this.props.category}/${product.id}`}
-                className="buttonProduct"
-              >
-                Ordering
-              </Link>
-            </div>
-          ))}
+        <div className={classes.root}>
+          <GridList cellHeight={180} className={classes.gridList}>
+            <GridListTile key="Subheader" cols={2} style={{ height: "auto" }}>
+              <ListSubheader component="div">Products</ListSubheader>
+            </GridListTile>
+            {this.props.products.map(product => (
+              <GridListTile key={product.id}>
+                <img src={product.image} alt={product.name} />
+                <GridListTileBar
+                  title={product.name}
+                  subtitle={<span>price:{product.price}</span>}
+                  actionIcon={
+                    <IconButton
+                      aria-lable={`info about ${product.name}`}
+                      className={classes.icon}
+                    >
+                      <InfoIcon />
+                    </IconButton>
+                  }
+                />
+              </GridListTile>
+            ))}
+          </GridList>
         </div>
       </div>
     );
