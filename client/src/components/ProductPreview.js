b6a94155300ca98150a0281b1cc76503f1ed8903
Amy Kim <amesk3@gmail.com>
2019-07-29 23:33:56 -0400

    productpreview updated

--- client/src/components/ProductPreview.js
+++ client/src/components/ProductPreview.js
@@ -5,36 +5,61 @@ const ProductPreview = props => {
     return <div>loading...</div>;
   }
   return (
-    <div>
+    <div classname="product-preview-div">
       <div key={props.selectedProduct.id}>
-        <h2>
-          <i>{props.selectedProduct.category}</i> >>{" "}
-          {props.selectedProduct.name}
-        </h2>
+        <h5>
+          <i>{props.selectedProduct.category}</i> > {props.selectedProduct.name}
+        </h5>
+        <div class="container-product-preview">
+          <div class="col">
+            <div class="row">
+              <img src={props.selectedProduct.image} style={{ width: "50%" }} />
+            </div>
+            <div class="row">
+              <h5>Price: ${props.selectedProduct.price}</h5>
+              <h5>{props.selectedProduct.description}</h5>
+              <form onSubmit={props.handleSubmit}>
+                <h5>
+                  Quantity
+                  <input
+                    style={{ width: "20%" }}
+                    type="number"
+                    name="quantity"
+                    value={props.state.quantity}
+                    onChange={props.handleChange}
+                    min="1"
+                  />
+                </h5>
 
-        <img src={props.selectedProduct.image} style={{ width: "200px" }} />
-        <p>${props.selectedProduct.price}</p>
-        <p>{props.selectedProduct.description}</p>
-
-        <form onSubmit={props.handleSubmit}>
-          <label>
-            Quantity
-            <input
-              style={{ width: "50px" }}
-              type="number"
-              name="quantity"
-              value={props.state.quantity}
-              onChange={props.handleChange}
-              min="1"
-            />
-          </label>
-          <button type="submit" className="cart">
-            Add to Cart
-          </button>
-          {props.state.addedtoCart[1] && (
-            <h3>{props.state.addedtoCart[0]} added to cart!</h3>
-          )}
-        </form>
+                <button type="submit" className="btn btn-success">
+                  Add to Cart
+                </button>
+                {props.state.addedtoCart[1] && (
+                  <h5>{props.state.addedtoCart[0]} added to cart!</h5>
+                )}
+              </form>
+            </div>
+            <div className="row">
+              <h5>Product Description</h5>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
